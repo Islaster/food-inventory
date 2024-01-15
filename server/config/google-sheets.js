63c6cd83a6976d1236a25d4309704fdb4ad5fb0f
Islaster <52678788+Islaster@users.noreply.github.com>
2024-01-14 22:49:00 -0800

    bug fix

--- server/config/google-sheets.js
+++ server/config/google-sheets.js
@@ -1,18 +1,18 @@
 const { google } = require("googleapis");
-const KEYFILEPATH = "../example-keyfile.json";
+const KEYFILEPATH = "../keyfile.json";
 
 const auth = new google.auth.JWT(
-    require(KEYFILEPATH).client_email,
-    null,
-    require(KEYFILEPATH).private_key,
-    ["https://www.googleapis.com/auth/spreadsheets"]
-  );
+  require(KEYFILEPATH).client_email,
+  null,
+  require(KEYFILEPATH).private_key,
+  ["https://www.googleapis.com/auth/spreadsheets"]
+);
 
-  auth.authorize((err, tokens) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-  });
-  
-  module.exports = auth;
+auth.authorize((err, tokens) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+});
+
+module.exports = auth;
